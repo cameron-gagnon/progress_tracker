5526c160caea2b5365f04c6c68288e3ad719b983
Cameron Gagnon <cameron.gagnon@gmail.com>
2017-05-18 12:26:12 -0500

    Don't track modifications

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
