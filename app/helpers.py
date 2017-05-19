3958e5415b0313db350f6437924a7a9ea49c9ba8
Cameron Gagnon <cameron.gagnon@gmail.com>
2017-05-19 09:31:08 -0500

    fix grade listing

--- app/helpers.py
+++ app/helpers.py
@@ -1,5 +1,5 @@
 colors = ["red", "orange", "yellow", "olive", "green", "teal", "blue", "violet", "purple", "pink", "brown", "grey", "black"]
-grades = ["6 y 7", "7 y 8", "10"]
+grades = ["6 y 7", "8 y 9", "10"]
 colorMap = {
     'Arduino': 'blue',
     '3D Modeling': 'red',
